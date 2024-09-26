98c4fb1e99900901dae51539df9fae7a79e2bca5
Nikita Ipatov <nikita@Nikitas-MacBook-Air.local>
2024-09-26 19:21:44 +0300

    feat: added submit btn

--- frontend/src/components/Button/SubmitButton/SubmitButton.tsx
+++ frontend/src/components/Button/SubmitButton/SubmitButton.tsx
@@ -1,7 +1,13 @@
-const SubmitButton = () => {
+interface ISubmitBtn {
+    isRequest: boolean;
+}
+
+const SubmitButton = (props: ISubmitBtn) => {
+    const { isRequest } = props;
+
     return (
-        <button>
-            
+        <button className="bg-blue-500 rounded-md px-6 py-3 text-white w-full hover:bg-blue-600 transition">
+            {!isRequest ? 'Submit' : 'Submitting...'}
         </button>
     );
 };
--- frontend/src/components/RegistrationForm/RegistrationForm.tsx
+++ frontend/src/components/RegistrationForm/RegistrationForm.tsx
@@ -1,12 +1,22 @@
-import InputPassword from "../Input/InputPassword/InputPassword";
-import InputText from "../Input/InputText/InputText";
+import { useState } from 'react';
+import SubmitButton from '../Button/SubmitButton/SubmitButton';
+import InputPassword from '../Input/InputPassword/InputPassword';
+import InputText from '../Input/InputText/InputText';
 
 const RegistrationForm = () => {
+    const [isRequest, setIsRequest] = useState(false);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        setIsRequest(true);
+        setTimeout(() => setIsRequest(false), 3000);
+    };
+
     return (
         <div className="flex flex-col items-center px-8 py-12 shadow-xl rounded-xl w-[450px]">
             <h1 className="text-3xl font-medium mb-10">Registration</h1>
-            <form className="w-full">
-                <div className="flex flex-col gap-2">
+            <form className="w-full flex flex-col items-center" onSubmit={handleSubmit}>
+                <div className="flex flex-col gap-2 w-full">
                     <InputText
                         id="email"
                         type="email"
@@ -23,6 +33,12 @@ const RegistrationForm = () => {
                         id={'confirm-password'}
                         placeholder="Confirm password"
                     />
+                </div>
+                <div className='mt-4 w-full flex flex-col items-center'>
+                    <SubmitButton isRequest={isRequest} />
+                    <span className="text-blue-500 cursor-pointer font-light mt-3">
+                        Login
+                    </span>
                 </div>
             </form>
         </div>
